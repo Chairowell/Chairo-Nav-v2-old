c824cd3d2520f1569bbc908cf7b702e64e6b0b48
是一只栗子啊 <chairowell@gmail.com>
2023-10-29 23:03:44 +0800

    更新 script.js

--- javascript/script.js
+++ javascript/script.js
@@ -358,19 +358,6 @@ function checkTime(obj,tim){
     }
 }
 
-// JSON File Reader
-function readTextFile(file, callback) {
-    var rawFile = new XMLHttpRequest();
-    rawFile.overrideMimeType("application/json");
-    rawFile.open("GET", file, false);
-    rawFile.onreadystatechange = function () {
-        if (rawFile.readyState === 4 && rawFile.status == "200") {
-            callback(rawFile.responseText);
-        }
-    }
-    rawFile.send(null);
-}
-
 document.addEventListener('DOMContentLoaded', function () {
 
     fetch('./poster.json')
